5991991ce25b58fab34d60e9442cb15b856a1a42
Michael Baldwin <baldwin@BaldwinMacbookPro.local>
2021-09-22 05:28:10 -0700

    Adds template project

---
+++ CovidActNow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidActNow
+//
+//  Created by Michael Baldwin on 9/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CovidActNow/CovidActNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidActNowApp.swift
+//  CovidActNow
+//
+//  Created by Michael Baldwin on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidActNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
