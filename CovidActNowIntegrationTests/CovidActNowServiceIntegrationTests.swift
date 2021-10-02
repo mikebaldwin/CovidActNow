62fc4bac4813984d8f0ed5ca5d1b3262e0010695
Michael Baldwin <baldwin@BaldwinMacbookPro.local>
2021-10-02 11:53:48 -0400

    Adds integration tests for fetching county data

--- CovidActNowIntegrationTests/CovidActNowServiceIntegrationTests.swift
+++ CovidActNowIntegrationTests/CovidActNowServiceIntegrationTests.swift
@@ -31,5 +31,32 @@ class CovidActNowServiceIntegrationTests: XCTestCase {
         XCTAssertEqual(stateData.fips.stringValue, "41")
         XCTAssertEqual(stateData.fips.intValue, 41)
         XCTAssertTrue(stateData.population > 1_000_000)
+    
+    func testGetCountyDataForMultnomah() async throws {
+        // Given
+        let service = CovidActNowService()
+        var expectedResult: LocationData?
+        
+        // When
+        let result = await service.getDataFor(county: .multnomah)
+        
+        switch result {
+        case let .success(resultData):
+            expectedResult = resultData
+        case let .failure(error):
+            XCTFail("Session failed with error: \(error)")
+        }
+
+        let countyData = try XCTUnwrap(expectedResult)
+        
+        // Then
+        XCTAssertEqual(countyData.fips.stringValue, "41051")
+        XCTAssertEqual(countyData.fips.intValue, 41051)
+        XCTAssertEqual(countyData.country, "US")
+        XCTAssertEqual(countyData.state, "OR")
+        XCTAssertEqual(countyData.county, "Multnomah County")
+        XCTAssertEqual(countyData.level, "county")
+        XCTAssertTrue(countyData.population > 0)
+        XCTAssertNotNil(countyData.metrics)
     }
 }
